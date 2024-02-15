dc856cb9fa6d2f1226b08d62ca883ca1d4e19be8
NeelPandruvada <26123550-NeelPandruvada@users.noreply.replit.com>
2024-02-15 23:54:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+import data from './paragraphs.json' assert { type: 'json' };
+
+const select = document.getElementById('paragraph');
+const p = document.getElementById('text');
+
+select.onchange = () => {
+  const sel = select.value;
+
+  if (sel == 'd') {
+    p.innerHTML = ' ';
+  } else {
+    try {
+      p.innerHTML = data[sel];
+    } catch (err) {
+      console.error(err);
+      p.innerHTML = ' ';
+    }
+  }
+};
