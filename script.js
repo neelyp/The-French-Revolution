61a8bc089ca7978e4a71b1c7430d7436d7c5ed78
neelyp <160043302+neelyp@users.noreply.github.com>
2024-02-15 20:32:06 -0500

    Clean up code

--- script.js
+++ script.js
@@ -6,14 +6,10 @@ const p = document.getElementById('text');
 select.onchange = () => {
   const sel = select.value;
 
-  // if (sel == 'd') {
-  //   p.innerHTML = ' ';
-  // } else {
   try {
     p.innerHTML = data[sel];
   } catch (err) {
     console.error(err);
     p.innerHTML = ' ';
   }
-  // }
 };
