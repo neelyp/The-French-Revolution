2619f329f6e768bd1cbd0c56aed7331904503d53
neelyp <160043302+neelyp@users.noreply.github.com>
2024-02-17 17:41:25 +0000

    Added images

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ import data from './paragraphs.json' assert { type: 'json' };
 
 const select = document.getElementById('paragraph');
 const p = document.getElementById('text');
+const img = document.getElementById('img-wrapper');
 
 select.onchange = () => {
   const sel = select.value;
 
   try {
-    p.innerHTML = data[sel];
+    p.innerHTML = data[sel].Paragraph;
+    img.innerHTML = `<img src="${data[sel].img}" alt="${data[sel]}" style="height: 25%; width: 25%;" />`
   } catch (err) {
     console.error(err);
     p.innerHTML = ' ';
