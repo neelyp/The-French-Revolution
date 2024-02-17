4b192332d656a2e8c178cc4d2d4d3fdde2b979bf
neelyp <160043302+neelyp@users.noreply.github.com>
2024-02-17 17:23:42 -0500

    change heading when options are selected

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ import data from './paragraphs.json' assert { type: 'json' };
 const select = document.getElementById('paragraph');
 const p = document.getElementById('text');
 const img = document.getElementById('img-wrapper');
+const title = document.querySelector('#title');
 
 select.onchange = () => {
   const sel = select.value;
 
   try {
     p.innerHTML = data[sel].Paragraph;
-    img.innerHTML = `<img src="${data[sel].img}" alt="${data[sel]}" style="height: 25%; width: 25%;" />`
+    img.innerHTML = `<img src="${data[sel].img}" alt="${data[sel]}" style="height: 28%; width: 30%;" />`;
+
+    title.innerHTML = select.options[select.selectedIndex].text;
   } catch (err) {
     console.error(err);
     p.innerHTML = ' ';
