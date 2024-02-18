4e075db2ca4a9b0db964db7283257ec5d31faae7
neelyp <160043302+neelyp@users.noreply.github.com>
2024-02-17 19:14:30 -0500

    made it work on safari

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-import data from './paragraphs.json' assert { type: 'json' };
+window.onload = async function () {
+  const response = await fetch('./paragraphs.json');
+  const data = await response.json();
 
-const select = document.querySelector('#paragraph');
-const p = document.querySelector('#text');
-const img = document.querySelector('#img-wrapper');
-const title = document.querySelector('#title');
+  const select = document.querySelector('#paragraph');
+  const p = document.querySelector('#text');
+  const img = document.querySelector('#img-wrapper');
+  const title = document.querySelector('#title');
 
-select.onchange = () => {
-  const sel = select.value;
+  select.onchange = () => {
+    const sel = select.value;
 
-  try {
-    p.innerHTML = data[sel].Paragraph;
-    img.innerHTML = `<img src="${data[sel].img}" alt="${data[sel]}" style="height: 28%; width: 30%;" />`;
+    try {
+      p.innerHTML = data[sel].Paragraph;
+      img.innerHTML = `<img src="${data[sel].img}" alt="${data[sel]}" style="height: 28%; width: 30%;" />`;
 
-    title.innerHTML = select.options[select.selectedIndex].text;
-  } catch (err) {
-    console.error(err);
-    p.innerHTML = ' ';
-  }
+      title.innerHTML = select.options[select.selectedIndex].text;
+    } catch (err) {
+      console.error(err);
+      p.innerHTML = ' ';
+    }
+  };
 };
